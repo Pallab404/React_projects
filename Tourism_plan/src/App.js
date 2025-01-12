bd3673e5e5a6189841b8b7349be5b0b1b2022fb7
Pallab Rajbongshi <pallabops07@gmail.com>
2025-01-12 20:38:42 +0530

    Tourism_plan

--- Tourism_plan/src/App.js
+++ Tourism_plan/src/App.js
@@ -1,11 +1,32 @@
-
+import {useState} from 'react';
+import Tour from './component/Tour.js';
+import data from './data.js';
 const App = () => {
 
-  return (
-    <div>
-    </div>
-  )
+  const [tours,setTours] = useState(data);
 
-};
+  const removeTour=(id)=>{
+    const newTours = tours.filter((tour)=> tour.id !== id);
+    setTours(newTours);
+  }
+
+  const refreshHandler=()=>{
+    setTours(data);
+  }
+
+  if (tours.length ===0) {
+    return(
+      <div className='refresh'>
+        <h2>No Tours Left </h2>
+        <button className='btn-white' onClick={refreshHandler}>Refresh</button>
+      </div>
+    )
+  }
+
+  return (
+      <div>
+        <Tour tours={tours} removeTour={removeTour}></Tour>
+      </div>
+  )};
 
 export default App;
